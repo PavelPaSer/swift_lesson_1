809d98a6bd35ae19ecc89849c19b6e392372892d
«Pashkov <pashkov921326@gmail.com>
2022-12-12 18:14:08 +0300

    3 задание

--- task_3.swift
+++ task_3.swift
@@ -129,5 +129,57 @@ let dictElCount = dict.count
 let arrayDict = Array(repeating: dictElCount, count: dictElCount)
 type(of: arrayDict)
  */
-                   
+
+
+/*
+СТРОКА коллекция символов.
+ 
+Задание 1
+1) Создайте переменную типа String со значением «Swift»
+2) Создайте константу типа Character с произвольным значением
+3) Создайте массив в соответсвии со следующими требованиями:
+— количество элементов должно соответствовать количеству символов
+— значения элементов должны быть такими же, как у константы из пункта 2
+— тип массива должен быть Array<String>
+4) Проиницилизируйте значение константы из пункта 2 в переменную из пункта 1
+
+var someStr = "SWIFT"
+let someChar: Character = "a"
+
+var Arr = Array(repeating: String(someChar), count: someStr.count)
+print(Arr) //["S", "S", "S", "S", "S"]
+type(of: Arr)
+someStr = String(someChar)
+
+ Задание 2
+ 1) Создайте константу со строковым значением "JonhWick"
+ 2) Получите индекс первого символа строки.
+ Используя полученный индекс выведите на консоль первый символ строки из пункта 1
+ 3) Одним выражением получите индекс последнего символа строки из пункта 1
+ Используя полученный индекс выведите на консоль последний символ строки из пункта 1
+ 4) Запишите в новую константу первые четыре символа из строки "БабаЯга"
+ Какой тип данных будет у созданной константы
+ 
+let newStr = "JonhWick"
+let secondIndex = newStr.index(newStr.startIndex, offsetBy: 0)
+print(newStr[secondIndex])
+let lastIndex = newStr.index(before: newStr.endIndex)
+newStr[lastIndex]
+newStr.unicodeScalars.count
+
+let newsStrg = "БабаЯга"
+let fristIndex = newsStrg.startIndex
+let charIndex = newsStrg.index(fristIndex, offsetBy: 3)
+let subStrt = newsStrg[fristIndex...charIndex]
+type(of: subStrt)
+
+
+Задание 3
+Самостоятельно в сети Интернет найдите кодовые точки для Unicode-символов «шахматные фигуры», после чего создайте два множества типа Set<Character>, состоящие из данных символов.
+Первое множество должно содержать все фигуры черного цвета, а второе — белого.
+ 
+let textWhite: Set<Character> = ["\u{2657}", "\u{2656}", "\u{2657}"]
+let textBlack: Set<Character> = ["\u{265A}", "\u{265B}", "\u{265C}"]
+ */
+         
                                           
